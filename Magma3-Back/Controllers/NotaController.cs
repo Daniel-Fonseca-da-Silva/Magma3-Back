e3646c24cfe88575439c218a96dc05a0f2196bf4
DanielFS <developer-web@programmer.net>
2023-04-25 13:56:41 -0300

    feat: Criado controller de nota

---
+++ Magma3-Back/Controllers/NotaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Magma3_Back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Hello", "World" };
+        }
+    }
+}
